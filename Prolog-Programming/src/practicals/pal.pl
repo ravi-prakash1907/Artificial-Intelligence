21d2415e0693f3a690b49b8eaf1526e933711c0d
ravi-prakash1907 <1907prakash.ravi@gmail.com>
2020-01-21 20:23:51 +0530

    palindrome

---
+++ Prolog-Programming/src/practicals/pal.pl
@@ -0,0 +1,18 @@
+/*  Palindrome  */
+
+/*   
+    1) reverse the list
+    2) compare 2 lists
+    */
+pal(L) :-
+    reverse(L, Reversed),
+    check(L, Reversed).
+
+check([], []) :-
+    write("Given list is Palindrome!"), nl.
+
+check([H|T1], [H|T2]) :-
+    check(T1, T2).
+
+check([_|_], [_|_]) :-
+    write("Given list is not Palindrome!!"), nl.
